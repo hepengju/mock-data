38abcc025c5500ec82a503ced968b37f3abb8d84
何鹏举 <hepengju1@jd.com>
2022-04-30 01:10:56 +0800

    优化英文名称生成器

--- src/main/java/com/hepengju/mockdata/generator/gen400_custom/gen410_name/EnglishNameGenerator.java
+++ src/main/java/com/hepengju/mockdata/generator/gen400_custom/gen410_name/EnglishNameGenerator.java
@@ -8,18 +8,22 @@ import org.springframework.core.annotation.Order;
 
 /**
  * 英文姓名生成器
- * 
- * @author hepengju
  *
+ * @author hepengju
  */
-@ApiModel("英文姓名生成器") @Data @Order(414)
+@ApiModel("英文姓名生成器")
+@Data
+@Order(414)
 public class EnglishNameGenerator extends AbstractStringGenerator {
 
-	private EnglishFirstNameGenerator firstName = new EnglishFirstNameGenerator();
-	private EnglishLastNameGenerator lastName = new EnglishLastNameGenerator();
+    private EnglishFirstNameGenerator firstName = new EnglishFirstNameGenerator();
+    private EnglishLastNameGenerator lastName = new EnglishLastNameGenerator();
 
-	@Override
-	public String generate() {
-		return firstName.generate() + " " + lastName.generate();
-	}
+    @Override
+    public String generate() {
+        double chance01 = Math.random();
+        if (chance01 < 0.3) return firstName.generate();
+        if (chance01 < 0.6) return lastName.generate();
+        return firstName.generate() + " " + lastName.generate();
+    }
 }
