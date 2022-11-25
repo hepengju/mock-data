96fb0ae17a4be5eef60a7e80093e3c7f43b9dff2
何鹏举 <he_pengju@163.com>
2022-11-25 18:44:58 +0800

    升级后的问题处理

--- src/main/java/com/hepengju/mockdata/MockDataApplication.java
+++ src/main/java/com/hepengju/mockdata/MockDataApplication.java
@@ -1,11 +1,10 @@
 package com.hepengju.mockdata;
 
-import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MybatisPlusAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MockDataApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hepengju/mockdata/config/Swagger2Config.java
+++ src/main/java/com/hepengju/mockdata/config/Swagger2Config.java
@@ -1,46 +1,46 @@
-package com.hepengju.mockdata.config;
-
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-/**
- * Swagger文档
- *
- * <br> <a href="https://doc.xiaominfo.com/">knife4j</a>: knife4j是为Java MVC框架集成Swagger生成Api文档的增强解决方案
- */
-@Configuration
-@EnableSwagger2
-@EnableKnife4j
-@Import(BeanValidatorPluginsConfiguration.class)
-public class Swagger2Config {
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hepengju.mockdata"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("喝可乐")
-                .description("接口文档")
-                .termsOfServiceUrl("https://gitee.com/hepengju/mock-data")
-                .contact(new Contact("何鹏举", "https://gitee.com/hepengju", "he_pengju@163.com"))
-                .version("1.0")
-                .build();
-    }
-}
+//package com.hepengju.mockdata.config;
+//
+//import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Import;
+//import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+///**
+// * Swagger文档
+// *
+// * <br> <a href="https://doc.xiaominfo.com/">knife4j</a>: knife4j是为Java MVC框架集成Swagger生成Api文档的增强解决方案
+// */
+//@Configuration
+//@EnableSwagger2
+//@EnableKnife4j
+//@Import(BeanValidatorPluginsConfiguration.class)
+//public class Swagger2Config {
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.hepengju.mockdata"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("喝可乐")
+//                .description("接口文档")
+//                .termsOfServiceUrl("https://gitee.com/hepengju/mock-data")
+//                .contact(new Contact("何鹏举", "https://gitee.com/hepengju", "he_pengju@163.com"))
+//                .version("1.0")
+//                .build();
+//    }
+//}
