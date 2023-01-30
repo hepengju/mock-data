e52c5d5b62d17975a34720fc1501ddadd025a72d
何鹏举 <he_pengju@163.com>
2023-01-30 16:18:16 +0800

    路由改为hash模式

--- front/src/main.js
+++ front/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue'
 
 import Home from './pages/Home.vue'
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes, 
 })
 
